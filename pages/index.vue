45fa32c88833f90ae9f4e1ed10ffdc3a534f630c
Manash Sonowal <manash149@gmail.com>
2023-01-13 00:12:22 +0530

    initial commit with nuxt and tailwind setup

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+<section class="bg-gray-900 text-white">
+  <div
+    class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
+  >
+    <div class="mx-auto max-w-3xl text-center">
+      <h1
+        class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+      >
+        ZUBEEN FAN CLUB HYDERABD
+
+        <span class="sm:block text-2xl"> WELCOMES YOU. </span>
+      </h1>
+
+      <h2
+        class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-lg font-extrabold text-transparent sm:text-4xl"
+      >
+        Join Us on 7th April 2023
+
+        <span class="sm:block text-6xl"> Zubeen Night</span>
+      </h2>
+
+      <p class="mx-auto mt-4 max-w-xl sm:text-xl sm:leading-relaxed">
+        Watch this space, for more information!
+      </p>
+
+      <div class="mt-8 flex flex-wrap justify-center gap-4">
+        <a
+          class="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+          href="mailto:admin@zfchyd.com?subject=Website Contact Us!"
+        >
+          Contact US
+        </a>
+
+        <!-- <a
+          class="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
+          href="/about"
+        >
+          Learn More
+        </a> -->
+      </div>
+    </div>
+  </div>
+</section>
+
+</template>
